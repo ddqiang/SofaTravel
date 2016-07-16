be711b154e1d14091b4d72608dd0a525a48abf8e
杨训诚 <1074362518@qq.com>
2016-07-16 21:27:39 +0800

    框架完成

--- app/src/main/java/com/example/dllo/sofatravel/main/main/base/BaseActivity.java
+++ app/src/main/java/com/example/dllo/sofatravel/main/main/base/BaseActivity.java
@@ -1,7 +1,29 @@
 package com.example.dllo.sofatravel.main.main.base;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by dllo on 16/7/16.
  */
-public class BaseActivity {
+public abstract class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayout());
+        initView();
+        initData();
+
+    }
+
+    //    加载布局的抽象方法
+    public abstract int getLayout();
+
+    //    初始化组件的抽象方法
+    public abstract void initView();
+
+    //    初始化数据的抽象方法
+    public abstract void initData();
+
 }
