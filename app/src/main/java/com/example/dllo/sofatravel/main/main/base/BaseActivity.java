8bd91bdd600eef7ac2d3d6e6bae8e5fbafd45629
mapingping <13591834720@139.com>
2016-07-18 08:37:05 +0800

    修改

--- app/src/main/java/com/example/dllo/sofatravel/main/main/base/BaseActivity.java
+++ app/src/main/java/com/example/dllo/sofatravel/main/main/base/BaseActivity.java
@@ -1,19 +1,11 @@
 package com.example.dllo.sofatravel.main.main.base;
-
-<<<<<<< HEAD
-import android.app.Activity;
-=======
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
->>>>>>> be711b154e1d14091b4d72608dd0a525a48abf8e
 
 /**
  * Created by dllo on 16/7/16.
  */
-<<<<<<< HEAD
-public class BaseActivity extends Activity {
-=======
 public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,9 +13,7 @@ public abstract class BaseActivity extends AppCompatActivity {
         setContentView(getLayout());
         initView();
         initData();
-
     }
-
     //    加载布局的抽象方法
     public abstract int getLayout();
 
@@ -32,6 +22,6 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     //    初始化数据的抽象方法
     public abstract void initData();
->>>>>>> be711b154e1d14091b4d72608dd0a525a48abf8e
+
 
 }
