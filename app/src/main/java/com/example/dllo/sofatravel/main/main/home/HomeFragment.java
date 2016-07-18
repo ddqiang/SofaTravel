14940a99514eb48149531621e97a3dd6bde96f42
杨训诚 <1074362518@qq.com>
2016-07-18 19:08:24 +0800

    home 写了一半

--- app/src/main/java/com/example/dllo/sofatravel/main/main/home/HomeFragment.java
+++ app/src/main/java/com/example/dllo/sofatravel/main/main/home/HomeFragment.java
@@ -1,7 +1,27 @@
 package com.example.dllo.sofatravel.main.main.home;
 
+import android.view.View;
+
+import com.example.dllo.sofatravel.R;
+import com.example.dllo.sofatravel.main.main.base.BaseFragment;
+
 /**
  * Created by dllo on 16/7/18.
  */
-public class HomeFragment {
+public class HomeFragment extends BaseFragment {
+
+    @Override
+    public int setLayout() {
+        return R.layout.fragment_home;
+    }
+
+    @Override
+    public void initView(View view) {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
 }
--- app/src/main/java/com/example/dllo/sofatravel/main/main/main/MainActivity.java
+++ app/src/main/java/com/example/dllo/sofatravel/main/main/main/MainActivity.java
@@ -7,10 +7,11 @@ import android.widget.RadioButton;
 
 import com.example.dllo.sofatravel.R;
 import com.example.dllo.sofatravel.main.main.base.BaseActivity;
+import com.example.dllo.sofatravel.main.main.home.HomeFragment;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
 
-    private RadioButton home, discover, order, mine;
+    private RadioButton home, discover, search, order, mine;
 
     @Override
     public int getLayout() {
@@ -23,8 +24,10 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         discover = (RadioButton) findViewById(R.id.discover_tab);
         order = (RadioButton) findViewById(R.id.order_tab);
         mine = (RadioButton) findViewById(R.id.mine_tab);
+        search = (RadioButton) findViewById(R.id.search_tab);
 
         home.setOnClickListener(this);
+        search.setOnClickListener(this);
         discover.setOnClickListener(this);
         order.setOnClickListener(this);
         mine.setOnClickListener(this);
@@ -32,23 +35,26 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     @Override
     public void initData() {
-        FragmentManager manager = getSupportFragmentManager();
-        FragmentTransaction transaction = manager.beginTransaction();
-        transaction.commit();
+
     }
 
     @Override
     public void onClick(View v) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
         switch (v.getId()) {
             case R.id.home_tab:
-
+                transaction.replace(R.id.show_fragment_fl, new HomeFragment());
                 break;
             case R.id.discover_tab:
+                break;
+            case R.id.search_tab:
                 break;
             case R.id.order_tab:
                 break;
             case R.id.mine_tab:
                 break;
         }
+        transaction.commit();
     }
 }
