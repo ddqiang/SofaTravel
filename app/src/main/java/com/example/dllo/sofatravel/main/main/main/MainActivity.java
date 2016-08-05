db734ed4970c8764d3170523844e57276da62407
杨训诚 <1074362518@qq.com>
2016-08-05 09:14:17 +0800

    收工 了 .

--- app/src/main/java/com/example/dllo/sofatravel/main/main/main/MainActivity.java
+++ app/src/main/java/com/example/dllo/sofatravel/main/main/main/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.dllo.sofatravel.main.main.main;
 
+import android.os.Build;
+import android.os.Handler;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.dllo.sofatravel.R;
 import com.example.dllo.sofatravel.main.main.base.BaseActivity;
@@ -46,7 +50,6 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         orderTv = (TextView) findViewById(R.id.order_tab_text);
         mineTv = (TextView) findViewById(R.id.mine_tab_text);
 
-
         mSearch.setOnClickListener(this);
         home.setOnClickListener(this);
         discover.setOnClickListener(this);
@@ -62,12 +65,12 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     @Override
     public void initData() {
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
         //透明状态栏
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         //透明导航栏
-//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-
+//            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
         FragmentManager managerOne = getSupportFragmentManager();
         FragmentTransaction transactionOne = managerOne.beginTransaction();
         transactionOne.replace(R.id.show_fragment_fl, new HomeFragment());
@@ -124,11 +127,48 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 homeTv.setTextColor(android.graphics.Color.parseColor("#C0C0C0"));
                 discoverTv.setTextColor(android.graphics.Color.parseColor("#C0C0C0"));
                 searchTv.setTextColor(android.graphics.Color.parseColor("#C0C0C0"));
-                mineTv.setTextColor(android.graphics.Color.parseColor("#C0C0C0"));
+                orderTv.setTextColor(android.graphics.Color.parseColor("#C0C0C0"));
                 mineTv.setTextColor(android.graphics.Color.parseColor("#A3D9DB"));
                 break;
         }
         transaction.commit();
+    }
+
+
+    /*
+        * 判断返回再一次退出
+        */
+    //标识是否退出
+    private boolean isExit;
+
+    //当按下物理按键的时候会调用此方法
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        //判断是否为返回键
+        if (keyCode == event.KEYCODE_BACK) {
+            //退出
+            exit();
+            return false;
+        } else {
+            return super.onKeyDown(keyCode, event);
+        }
+    }
+
+    private void exit() {
+        if (!isExit) {
+            isExit = true;
+            Toast.makeText(this, "再按一次返回退出", Toast.LENGTH_SHORT).show();
+            //使用Handler延时3秒发送(postDelayed)
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    isExit = false;
+                }
+            }, 3000);
+        } else {
+            finish();
+            System.exit(0);
+        }
     }
 
 
